a0d9c536d5e182e623a787f4e3ab3245688922ac
Yassemine <yassemine.alaoui@etu.u-paris.fr>
2026-02-06 18:00:47 +0100

    Ajout Contact stub et implementation InMemoryRepository

---
+++ src/main/java/fr/eidd/salesapp/domain/Contact.java
@@ -0,0 +1,10 @@
+package fr.eidd.salesapp.domain;
+
+// CECI EST UN BOUCHON (Stub) pour permettre de coder le Repository.
+// Il sera remplacé plus tard 
+public class Contact {
+    private Long id;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+}
