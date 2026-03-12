a10c0f3a50b59f9700420c2297d900118856236c
Yassemine <yassemine.alaoui@etu.u-paris.fr>
2026-03-12 12:42:22 +0100

    Configuration Maven : ajout JUnit, Mockito, JaCoCo et test minimal

---
+++ src/test/java/ConfigurationTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ConfigurationTest {
+    @Test
+    void shouldPassSimpleTest() {
+        assertTrue(true);
+    }
+}
