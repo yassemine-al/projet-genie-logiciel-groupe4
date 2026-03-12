58d6d9077f980f226f4d579989ce2329f8a49f65
rosawafall <awafall1209@gmail.com>
2026-03-12 11:45:11 +0100

    Ajout de la fonction MemoryContact

--- projet/addressBook/repository/MemoryContact.java
+++ projet/addressBook/repository/MemoryContact.java
@@ -1,5 +1,40 @@
 package addressBook.repository;
 
-public class MemoryContact implements ContactRepository{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import adressBook.entités.Contact;
 
+
+public class MemoryContact implements ContactRepository {
+
+    private final List<Contact> contacts = new ArrayList<>();
+
+    @Override
+    public Contact save(Contact contact) {
+        // Ajout d'un contact à la liste
+        this.contacts.add(contact);
+        return contact;
+    }
+
+    @Override
+    public List<Contact> findAll() {
+        // nouvelle liste contenant tous les contacts
+        return new ArrayList<>(this.contacts);
+    }
+
+    @Override
+    public Optional<Contact> findById(Long id) {
+        // recherche du contact qui a l'ID correspondant
+        return contacts.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public void delete(Long id) {
+        // On supprime le contact si l'ID correspond
+        contacts.removeIf(c -> c.getId().equals(id));
+    }
 }
+
